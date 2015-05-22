ca333ec658321b4ac20f6089a60e766d08397a16
Iliyan Trifonov <iliyan.trifonov@gmail.com>
2015-05-22 15:48:51 +0300

    index route tests

--- test/server/routes/index_test.js
+++ test/server/routes/index_test.js
@@ -1,1 +1,17 @@
-//to be done
+var indexRoute = require('../../../src/routes/index'),
+    assert = require('assert');
+
+describe('index route\'s .index()', function () {
+    var req = {
+            user: 'Test user object'
+        },
+        res = {
+            render: function (template, user) {
+                assert.strictEqual(template, 'index');
+                assert.deepEqual(user, { user: req.user });
+            }
+        };
+    it('should call to render the index page with the logged in user', function () {
+        indexRoute.index(req, res);
+    });
+});
