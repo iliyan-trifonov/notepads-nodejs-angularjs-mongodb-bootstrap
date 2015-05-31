f678b9fcb9182acf4b91e2f275a1e444e7bfd74b
Iliyan Trifonov <iliyan.trifonov@gmail.com>
2015-05-31 13:35:38 +0300

    WIP: users route tests

--- test/server/routes/users_test.js
+++ test/server/routes/users_test.js
@@ -25,4 +25,31 @@ describe('Users Routes', function () {
         });
     });
 
+    describe('postAuthHandler', function () {
+
+        it('should return BAD REQUEST with not enough params given', function (done) {
+            var req = {
+                body: {
+                    fbId: '',
+                    fbAccessToken: '',
+                    accessToken: ''
+                }
+            };
+
+            var res = {
+                status: function status (stat) {
+                    assert.strictEqual(stat, HttpStatus.BAD_REQUEST);
+                    return this;
+                },
+                json: function json (data) {
+                    assert.deepEqual(data, {});
+                    done();
+                }
+            };
+
+            usersRouter.postAuthHandler(req, res);
+        });
+
+    });
+
 });
