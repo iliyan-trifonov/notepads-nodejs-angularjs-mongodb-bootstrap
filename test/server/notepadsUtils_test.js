efe4906a69b88de7186677afb2bff9960ebe50a0
Iliyan Trifonov <iliyan.trifonov@gmail.com>
2015-06-04 14:38:06 +0300

    converted to promises

--- test/server/notepadsUtils_test.js
+++ test/server/notepadsUtils_test.js
@@ -12,14 +12,14 @@ describe('notepadsUtils', function () {
 
     var db, user;
 
-    before(function (done) {
+    before(function () {
         //TODO: use callback/promise:
         db = connection();
 
         var fbId = String(+new Date()), //convert to string for the comparison below
             name = 'Iliyan Trifonov',
             photo = 'photourl';
-        User.createAsync({
+        return User.createAsync({
             facebookId: fbId,
             name: name,
             photo: photo
@@ -28,11 +28,11 @@ describe('notepadsUtils', function () {
             assert.strictEqual(doc.name, name);
             assert.strictEqual(doc.photo, photo);
             user = doc;
-        }).then(done);
+        });
     });
 
-    after(function (done) {
-        User.removeAsync({})
+    after(function () {
+        return User.removeAsync({})
             .then(function () {
                 return Category.remove({});
             })
@@ -41,36 +41,30 @@ describe('notepadsUtils', function () {
             })
             .then(function () {
                 db.close();
-            })
-            .then(done);
+            });
     });
 
-    it("should create a new 'Sample Category' and a new Notepad 'Read me' for a given user id", function (done) {
-        notepadsUtils.prepopulate(user._id)
+    it("should create a new 'Sample Category' and a new Notepad 'Read me' for a given user id", function () {
+        return notepadsUtils.prepopulate(user._id)
             .then(function (result) {
-                console.info('result', result);
                 assert.ok(result.user);
                 assert.ok(result.category);
                 assert.ok(result.notepad);
                 //TODO: check if the category and notepad exist in db and their props/texts are as expected
-            })
-            .then(done);
+            });
     });
 
-    it('should return Error Invalid user id on missing uid param', function (done) {
-        notepadsUtils.prepopulate(null).catch(function (err) {
+    it('should return Error Invalid user id on missing uid param', function () {
+        return notepadsUtils.prepopulate(null).catch(function (err) {
             assert.ok(err);
             assert.strictEqual(err.message, 'Invalid user id!');
-            done();
         });
     });
 
-    it('should return Error on not existing user with the given uid', function (done) {
-        notepadsUtils.prepopulate(mongoose.Types.ObjectId(+new Date()))
+    it('should return Error on not existing user with the given uid', function () {
+        return notepadsUtils.prepopulate(mongoose.Types.ObjectId(+new Date()))
             .catch(function (err) {
-                console.log('err', err);
                 assert.strictEqual(err.message, 'User not found!');
-                done();
             });
     });
 
