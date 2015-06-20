14775bb67a79437fb886480d30f60742ed0c4cc9
Iliyan @ Asus Tablet <iliyan.trifonov@gmail.com>
2015-06-21 02:28:46 +0300

    refactored to ES6 code

--- src/db.js
+++ src/db.js
@@ -1,19 +1,19 @@
 'use strict';
 
-var mongoose = require('mongoose'),
+let mongoose = require('mongoose'),
     config;
 
-var uri;
-var options = {
+let uri;
+let options = {
     db: { native_parser: true },
     server: { poolSize: 5 },
     keepAlive: true //this option may have changed its place in this object
 };
 
-module.exports = exports = function () {
-    var db = mongoose.connect(uri, options);
+module.exports = exports = () => {
+    let db = mongoose.connect(uri, options);
 
-    db.connection.on('error', function (err) {
+    db.connection.on('error', err => {
         process.stderr.write(err);
         process.exit(1);
     });
@@ -21,8 +21,8 @@ module.exports = exports = function () {
     return db.connection;
 };
 
-module.exports.setAppConfig = exports.setAppConfig = function (cnf) {
+module.exports.setAppConfig = exports.setAppConfig = cnf => {
+    console.info('db - using config:', cnf);
     config = cnf;
-    console.log('db: using config - ', config);
     uri = config.mongodb.uri;
 };
