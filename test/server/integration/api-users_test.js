aeaeeca8631ccef894d2e8cdd473d2eb17ada4de
Iliyan @ Asus Tablet <iliyan.trifonov@gmail.com>
2015-06-23 00:24:30 +0300

    automatic FB access token and test user handling, automatic skipping of the tests that require test user and access token if it can't be generated

--- test/server/integration/api-users_test.js
+++ test/server/integration/api-users_test.js
@@ -5,9 +5,18 @@ var request = require('supertest'),
     HttpStatus = require('http-status'),
     config,
     connection = require('../../db_common'),
-    assert = require('assert');
+    assert = require('assert'),
+    graph = require('fbgraph'),
+    Promise = require('bluebird'),
+    co = require('co');
 
 import User from '../../../src/models/user';
+
+Promise.promisify(graph.get);
+
+if (!graph.getAsync) {
+    graph.getAsync = Promise.promisify(graph.get);
+}
 
 try {
     config = require('../../../config/app.conf.json');
@@ -19,21 +28,40 @@ describe('API /users', function () {
 
     var db, testUser;
 
-    before(function (done) {
-        db = connection();
+    before(() => {
+        return co(function* () {
+            db = connection();
 
-        return User.createAsync({
-            'facebookId': +new Date(),
-            'name': 'Iliyan Trifonov',
-            'photo': 'photourl'
-        }).then(function (user) {
-            testUser = user;
-        }).then(done);
+            testUser = yield User.createAsync({
+                'facebookId': +new Date(),
+                'name': 'Iliyan Trifonov',
+                'photo': 'photourl'
+            });
+
+            //if configuration is provided, enable tests that require real FB user id and access token:
+            if (config.facebook.app.id && config.facebook.app.secret) {
+                console.log('using facebook app id and secret', config.facebook.app.id, config.facebook.app.secret);
+                try {
+                    let result = yield graph.getAsync([
+                        '/',
+                        config.facebook.app.id,
+                        '/accounts/test-users?access_token=',
+                        config.facebook.app.id,
+                        '|',
+                        config.facebook.app.secret
+                    ].join(''));
+
+                    console.log('result', result);
+                    if (result && result.data && result.data.length > 0) {
+                        config.testFBUser.fbId = result.data[0].id;
+                        config.testFBUser.fbAccessToken = result.data[0].access_token;
+                    }
+                } catch (err) {/**/}
+            }
+        });
     });
 
-    after(function () {
-        db.close();
-    });
+    after(() => db.close());
 
     describe('/auth', function () {
 
@@ -84,70 +112,78 @@ describe('API /users', function () {
                 });
         });
 
-        //these tests pass only with a given test fb user - private CI or locally
-        if (config.testFBUser && config.testFBUser.fbAccessToken) {
-            /*
-             it('should return FORBIDDEN if no FB user is found by the given fbAccessToken', function (done) {
-             var wrongToken = 'we need a valid FB token that could not find a user';
-             request(app)
-             .post(url)
-             .send({
-             fbId: config.testFBUser.fbId,
-             fbAccessToken: wrongToken
-             })
-             .expect('Content-Type', /json/)
-             .expect(HttpStatus.FORBIDDEN, done);
-             });
-             */
+////////these tests pass only with a given real test fb user - test on private CI or locally://////////////////////////
 
-            it('should return FORBIDDEN if FB user is found but has different id', function (done) {
+        it('should return FORBIDDEN if FB user is found but has different id', function (done) {
+            if (!config.testFBUser || !config.testFBUser.fbAccessToken) {
+                this.skip();
+            }
+
+            request(app)
+                .post(url)
+                .send({
+                    fbId: +new Date(),
+                    fbAccessToken: config.testFBUser.fbAccessToken
+                })
+                .expect('Content-Type', /json/)
+                .expect(HttpStatus.FORBIDDEN, done);
+        });
+
+        //TODO: check for the prepopulated cat and notepad here too:
+        it('should return a valid accessToken if FB user is created by the given fbAccessToken', function (done) {
+            if (!config.testFBUser || !config.testFBUser.fbAccessToken) {
+                this.skip();
+            }
+
+            //remove the user if exists
+            User.removeAsync({
+                facebookId: config.testFBUser.fbId
+            }).then(() => {
                 request(app)
                     .post(url)
                     .send({
-                        fbId: +new Date(),
+                        fbId: config.testFBUser.fbId,
                         fbAccessToken: config.testFBUser.fbAccessToken
                     })
                     .expect('Content-Type', /json/)
-                    .expect(HttpStatus.FORBIDDEN, done);
+                    .expect(HttpStatus.OK)
+                    .end(function (err, result) {
+                        assert.ifError(err);
+                        assert.ok(result.body.accessToken);
+                        done();
+                    });
             });
+        });
 
-            it('should return a valid accessToken if FB user is found in DB', function (done) {
-                User.createAsync({
-                    name: 'Iliyan Trifonov',
-                    facebookId: config.testFBUser.fbId,
-                    photo: 'photourl'
-                }).then(function (user) {
-                    request(app)
-                        .post(url)
-                        .send({
-                            fbId: config.testFBUser.fbId,
-                            fbAccessToken: config.testFBUser.fbAccessToken
-                        })
-                        .expect('Content-Type', /json/)
-                        .expect(HttpStatus.OK)
-                        .end(function (err, res) {
-                            assert.ifError(err);
-                            assert.strictEqual(res.body.accessToken, user.accessToken);
-                            done();
-                        });
-                });
-            });
+        it('should return a valid accessToken if FB user is found in DB', function (done) {
+            if (!config.testFBUser || !config.testFBUser.fbAccessToken) {
+                this.skip();
+            }
 
-            //TODO: check for the prepopulated cat and notepad here too:
-            it('should return a valid accessToken if FB user is created by the given fbAccessToken', function (done) {
-                User.removeAsync({
-                    facebookId: config.testFBUser.fbId
-                }).then(function () {
-                    request(app)
-                        .post(url)
-                        .send({
-                            fbId: config.testFBUser.fbId,
-                            fbAccessToken: config.testFBUser.fbAccessToken
-                        })
-                        .expect('Content-Type', /json/)
-                        .expect(HttpStatus.OK, done);
-                });
+            //the user should be created by the previous test
+            User.findOneAsync({
+                facebookId: config.testFBUser.fbId
+            }).then(user => {
+                assert.ok(user);
+                assert.ok(user.accessToken);
+
+                request(app)
+                    .post(url)
+                    .send({
+                        fbId: config.testFBUser.fbId,
+                        fbAccessToken: config.testFBUser.fbAccessToken
+                    })
+                    .expect('Content-Type', /json/)
+                    .expect(HttpStatus.OK)
+                    .end((err, res) => {
+                        assert.ifError(err);
+                        assert.strictEqual(res.body.accessToken, user.accessToken);
+                        done();
+                    });
             });
-        }
+        });
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
     });
 });
