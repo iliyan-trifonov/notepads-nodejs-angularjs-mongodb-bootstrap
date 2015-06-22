0f0e0107d6aaf7382dd88dd3b940726e4cb63862
Iliyan @ Asus Tablet <iliyan.trifonov@gmail.com>
2015-06-23 00:21:12 +0300

    used ES6 import/export/let, added more error checking/handling

--- src/notepadsUtils.js
+++ src/notepadsUtils.js
@@ -1,76 +1,85 @@
 'use strict';
 
-let Category = require('./models/category'),
-    Notepad = require('./models/notepad'),
-    Promise = require('bluebird');
-
 import User from './models/user';
+import Category from './models/category';
+import Notepad from './models/notepad';
+import Promise from 'bluebird';
+import co from 'co';
 
 module.exports.prepopulate = exports.prepopulate = uid => {
+    let user, userOrig, category, notepad;
 
-    if (!uid) {
-        return Promise.reject(new Error('Invalid user id!'));
-    }
+    return co(function* () {
+        if (!uid) {
+            throw new Error('Invalid user id!');
+        }
 
-    //TODO: delete the notepad/category on any error / transaction?
+        user = userOrig = yield User.findOneAsync({ _id: uid });
 
-    let user, category, notepad;
+        if (!user) {
+            throw new Error('User not found!');
+        }
 
-    return User.findOneAsync({ _id: uid })
-        .then(foundUser => {
-            if (!foundUser) {
-                let msg = 'User not found!';
-                console.error(msg);
-                return Promise.reject(new Error(msg));
-            }
-            user = foundUser;
-            return Category.createAsync({
-                'name': 'Sample category',
-                'user': uid
-            });
-        })
-        .then(cat => {
-            category = cat;
-            return User.addCategory(user._id, cat._id);
-        })
-        .then(updatedUser =>
-            Notepad.createAsync({
-                title: 'Read me',
-                text: 'Use the menu on the top left to create your own categories ' +
-                'and then add notepads to them.' + "\n\n" +
-                'On the top right of the Dashboard there is a plus sign inside a circle.' +  "\n" +
-                'Tapping on it will open the Add Notepad window.' +  "\n" +
-                'Select a category, provide a title and text and save the new notepad.' +  "\n\n" +
-                'The smaller plus signs on the right of each category\'s name will open the same ' +
-                'Add Notepad window with the category preselected.' +  "\n\n" +
-                'Click on a Notepad on the Dashboard and you will go to the View Notepad window ' +
-                'where you can read it, edit it and delete it.' +  "\n\n" +
-                'The categories are managed from their own window where you can go ' +
-                'by selecting Categories from the left menu(top left to open it).' +  "\n\n" +
-                'Be careful when deleting a category as this will delete all notepads in it.' + "\n\n",
-                category: category._id,
-                user: updatedUser._id
-            })
-        )
-        .then(notepadCreated => {
-            notepad = notepadCreated;
-            return User.addNotepad(user._id, notepadCreated._id);
-        })
-        .then(updatedUser => {
-            user = updatedUser;
-            return Category.increaseNotepadsCountById(category._id);
-        })
-        .then(cat =>
-            ({
-                user: user,
-                category: cat,
-                notepad: notepad
-            })
-        )
-        //catch may be skipped here and errors caught later on code calling prepopulate()
-        .catch(err => {
-            console.error(err);
-            //reject again for other catch()-es on the chain
-            return Promise.reject(err);
+        category = yield Category.createAsync({
+            'name': 'Sample category',
+            'user': user._id
         });
+
+        user = yield User.addCategory(user._id, category._id);
+
+        notepad = yield Notepad.createAsync({
+            title: 'Read me',
+            text: 'Use the menu on the top left to create your own categories ' +
+            'and then add notepads to them.' + "\n\n" +
+            'On the top right of the Dashboard there is a plus sign inside a circle.' +  "\n" +
+            'Tapping on it will open the Add Notepad window.' +  "\n" +
+            'Select a category, provide a title and text and save the new notepad.' +  "\n\n" +
+            'The smaller plus signs on the right of each category\'s name will open the same ' +
+            'Add Notepad window with the category preselected.' +  "\n\n" +
+            'Click on a Notepad on the Dashboard and you will go to the View Notepad window ' +
+            'where you can read it, edit it and delete it.' +  "\n\n" +
+            'The categories are managed from their own window where you can go ' +
+            'by selecting Categories from the left menu(top left to open it).' +  "\n\n" +
+            'Be careful when deleting a category as this will delete all notepads in it.' + "\n\n",
+            category: category._id,
+            user: user._id
+        });
+
+        category = yield Category.increaseNotepadsCountById(category._id);
+
+        user = yield User.addNotepad(user._id, notepad._id);
+
+        return {
+            user: user,
+            category: category,
+            notepad: notepad
+        };
+    }).catch(err => {
+        //cleanup - restore:
+        //TODO: test this cleanup
+        if (category && category._id) {
+            Category.remove({ _id: category._id });
+        }
+
+        if (notepad && notepad._id) {
+            Notepad.remove({ _id: notepad._id });
+        }
+
+        if (userOrig && userOrig._id) {
+            User.findOneAndUpdate(
+                {_id: userOrig._id},
+                {
+                    $set: {
+                        categories: userOrig.categories,
+                        notepads: userOrig.notepads
+                    }
+                }
+            );
+        }
+        //
+
+        console.error(err);
+        //reject again for other catch()-es on the chain
+        return Promise.reject(err);
+    });
 };
