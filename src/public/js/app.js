b3b54665a45d43c58df21b90c61400dc9f6a776d
Iliyan Trifonov <iliyan.trifonov@gmail.com>
2015-08-02 12:31:24 +0300

    loaded ngAnimate and added an interceptor to put the accessToken in the request headers - provides the option to use a remote API and separate the front-end and back-end

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -8,57 +8,67 @@
         'Notepads.vars',
         'ngRoute',
         'ngSanitize',
-        'ui.bootstrap'
+        'ui.bootstrap',
+        'ngAnimate'
     ])
-    .config(['$routeProvider', '$locationProvider', 'USER_CONTEXT', function ($routeProvider, $locationProvider, USER_CONTEXT) {
+    .config([
+        '$routeProvider', '$locationProvider', 'USER_CONTEXT', '$httpProvider',
+        function ($routeProvider, $locationProvider, USER_CONTEXT, $httpProvider) {
+            if (USER_CONTEXT && USER_CONTEXT.id) {
+                $routeProvider.when('/', {
+                    templateUrl: '/partials/dashboard.html',
+                    controller: 'DashboardCtrl'
+                })
+                .when('/notepads/add', {
+                    templateUrl: '/partials/notepad.html',
+                    controller: 'NotepadCtrl'
+                })
+                .when('/notepads/add/catid/:cid', {
+                    templateUrl: '/partials/notepad.html',
+                    controller: 'NotepadCtrl'
+                })
+                .when('/notepads/:id/edit', {
+                    templateUrl: '/partials/notepad.html',
+                    controller: 'NotepadCtrl'
+                })
+                .when('/notepads/:id/delete', {
+                    templateUrl: '/partials/notepad-delete.html',
+                    controller: 'NotepadDelCtrl'
+                })
+                .when('/categories', {
+                    templateUrl: '/partials/categories.html',
+                    controller: 'CategoriesCtrl'
+                })
+                .when('/categories/add', {
+                    templateUrl: '/partials/category.html',
+                    controller: 'CategoryCtrl'
+                })
+                .when('/categories/:id/edit', {
+                    templateUrl: '/partials/category.html',
+                    controller: 'CategoryCtrl'
+                })
+                .when('/categories/:id/delete', {
+                    templateUrl: '/partials/category-delete.html',
+                    controller: 'CategoryDelCtrl'
+                });
+            } else {
+                $routeProvider.when('/', {
+                    templateUrl: '/partials/index.html',
+                    controller: 'IndexCtrl'
+                });
+            }
 
-        if (USER_CONTEXT.id) {
-            $routeProvider.when('/', {
-                templateUrl: '/partials/dashboard.html',
-                controller: 'DashboardCtrl'
-            })
-            .when('/notepads/add', {
-                templateUrl: '/partials/notepad.html',
-                controller: 'NotepadCtrl'
-            })
-            .when('/notepads/add/catid/:cid', {
-                templateUrl: '/partials/notepad.html',
-                controller: 'NotepadCtrl'
-            })
-            .when('/notepads/:id/edit', {
-                templateUrl: '/partials/notepad.html',
-                controller: 'NotepadCtrl'
-            })
-            .when('/notepads/:id/delete', {
-                templateUrl: '/partials/notepad-delete.html',
-                controller: 'NotepadDelCtrl'
-            })
-            .when('/categories', {
-                templateUrl: '/partials/categories.html',
-                controller: 'CategoriesCtrl'
-            })
-            .when('/categories/add', {
-                templateUrl: '/partials/category.html',
-                controller: 'CategoryCtrl'
-            })
-            .when('/categories/:id/edit', {
-                templateUrl: '/partials/category.html',
-                controller: 'CategoryCtrl'
-            })
-            .when('/categories/:id/delete', {
-                templateUrl: '/partials/category-delete.html',
-                controller: 'CategoryDelCtrl'
+            $routeProvider.otherwise({ redirectTo: '/' });
+
+            $locationProvider.html5Mode(true);
+
+            $httpProvider.interceptors.push(function() {
+                return {
+                    'request': function(config) {
+                        config.headers['x-access-token'] = USER_CONTEXT.accessToken;
+                        return config;
+                    }
+                };
             });
-        } else {
-            $routeProvider.when('/', {
-                templateUrl: '/partials/index.html',
-                controller: 'IndexCtrl'
-            });
-        }
-
-        $routeProvider.otherwise({ redirectTo: '/' });
-
-        $locationProvider.html5Mode(true);
-
     }]);
 })(angular);
